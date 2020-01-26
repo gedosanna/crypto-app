44038ec5da1d8f2d648cba2a7f2974d0e5a1e16f
gedosanna <56969611+gedosanna@users.noreply.github.com>
2020-01-26 16:41:28 +0100

    Updates

---
+++ precache-manifest.a06e95ef67cc657d77c7ee77e4529cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5b6bbefb3894cdceadde737840a5ca",
+    "url": "/crypto-app/index.html"
+  },
+  {
+    "revision": "c51ac8224c17d09750b2",
+    "url": "/crypto-app/static/css/main.feb29e28.chunk.css"
+  },
+  {
+    "revision": "8bc9e18611c4753b0e3f",
+    "url": "/crypto-app/static/js/2.27c0cb5b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/crypto-app/static/js/2.27c0cb5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c51ac8224c17d09750b2",
+    "url": "/crypto-app/static/js/main.4eca5492.chunk.js"
+  },
+  {
+    "revision": "8244145dd655b7c92522",
+    "url": "/crypto-app/static/js/runtime-main.a54917cb.js"
+  }
+]);
